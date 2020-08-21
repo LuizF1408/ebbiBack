d4c1d023145780347a5ebb45ff15b078aab138e9
Luiz F <liko.12@hotmail.com>
2020-08-20 23:20:28 -0300

    cadastro de usuários com validação e token

--- src/routes/postRoutes/routes.js
+++ src/routes/postRoutes/routes.js
@@ -48,31 +48,7 @@ router.post('/signup',async(req,res)=> {
 //     return true;
 // };
 
-// router.post('/signUp', async (req, res) => {
-//     const username = req.body.username;
-//     const password = req.body.password;
-//     const user = await User.findOne({ username: username });
-//     const pswd = await User.findOne({ password: password });
-//     if (user) {
-//         res.status(200).json({ message: 'Already exists' });
-//         return;
-//     }
-//     if (pswd) {
-//         res.status(200).json({ message: 'Wrong password'})
-//         return
-//     }
 
-//     const newUser = new User({
-//         email: username,
-//         password: password,
-//     });
-//     try {
-//         newUser.save()
-//         res.status(201).json({ message: 'User created successfully' });
-//     } catch (error) {
-//         res.status(400).json({ message: error.message });
-//     }
-// });
 
 router.post('/newCard', async (req, res) => {
     // if(!await passwordAuthorizated(req, res)) return
