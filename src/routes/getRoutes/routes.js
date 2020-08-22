f008285828e0b2bfdca460600f6e14fa0098eec3
HCFerraz <henriquecabralferraz@gmail.com>
2020-08-21 22:02:03 -0300

    Rota decks

--- src/routes/getRoutes/routes.js
+++ src/routes/getRoutes/routes.js
@@ -2,12 +2,23 @@ require('../../../database')
 const express = require('express')
 const router = express.Router()
 
-const User = require('../../schemas/userSchema');
+const User = require('../../models/userSchema');
+const { Deck }= require('../../models/dataSchema')
 
 router.get('/seeAllUsers', async (req, res) => {
     const users = await User.find()
     if (users.length > 0) {
         res.status(200).json(users)
+    }
+    else {
+        res.status(404).json({ message: 'Nenhum dado encontrado' })
+    }
+})
+
+router.get('/seeAllDecks', async (req, res) => {
+    const decks = await Deck.find()
+    if(decks.length > 0) {
+        res.status(200).json(decks)
     }
     else {
         res.status(404).json({ message: 'Nenhum dado encontrado' })
